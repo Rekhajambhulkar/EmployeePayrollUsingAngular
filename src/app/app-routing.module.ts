c4d92ac63a369b8785de82b81426275ca2ba7511
RekhaJambhulkar <jambhulkarrekha@gmail.com>
2021-05-15 19:00:29 +0530

    Added: create Dashboard UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
-
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo:'/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
